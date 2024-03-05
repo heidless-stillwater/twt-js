49f2851c0e7f6a5d7ae41242214cbfce8a41fb9d
heidless <rob.lockhart@yahoo.co.uk>
2024-03-05 12:11:12 +0000

    init

---
+++ project.js
@@ -0,0 +1,11 @@
+// 1. deposit some money
+// 2. determine a number of lines to bet on
+// 3. collect a bet amount
+// 4. spin slot machine
+// 5. check if user won
+// 6. give user their winnings
+// 7. play again
+
+function deposit() {
+    
+}
